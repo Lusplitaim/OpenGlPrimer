e2c0bd29b3ef0ecd5bd93ccaea45fbeb5c308689
Lusplitaim <Foolman.ru@yandex.ru>
2025-07-25 14:39:34 +0300

    vertex and fragment shaders

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using Silk.NET.Core.Contexts;
 using Silk.NET.GLFW;
 using Silk.NET.OpenGL;
-using GLBuffer = Silk.NET.OpenGL.Buffer;
 
 namespace OpenGlPrimer
 {
@@ -18,9 +17,15 @@ namespace OpenGlPrimer
 
             var window = InitWindow(glfw);
 
+            var gl = GL.GetApi(new GlfwContext(glfw, window));
+            gl.Viewport(0, 0, 640, 480);
+            InitVertices(gl);
+
             while (!glfw.WindowShouldClose(window))
             {
                 glfw.PollEvents();
+                gl.Clear((int)GLEnum.ColorBufferBit);
+                gl.DrawArrays(GLEnum.Triangles, 0, 3);
                 glfw.SwapBuffers(window);
             }
 
@@ -46,6 +51,60 @@ namespace OpenGlPrimer
             glfw.MakeContextCurrent(window);
 
             return window;
+        }
+
+        static unsafe void InitVertices(GL gl)
+        {
+            float[] vertices = [
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f, 0.5f, 0.0f
+            ];
+
+            gl.GenVertexArrays(1, out uint vao);
+            gl.BindVertexArray(vao);
+
+            gl.GenBuffers(1, out uint vbo);
+            gl.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+            gl.BufferData<float>(BufferTargetARB.ArrayBuffer, (nuint)(vertices.Length * sizeof(float)), vertices, BufferUsageARB.StaticDraw);
+
+            uint vShader = gl.CreateShader(GLEnum.VertexShader);
+            string vShaderSrc = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vShader.vert"));
+            gl.ShaderSource(vShader, vShaderSrc);
+            gl.CompileShader(vShader);
+            if ((GLEnum)gl.GetShader(vShader, GLEnum.CompileStatus) == GLEnum.False)
+            {
+                gl.GetShaderInfoLog(vShader, 512, null, out string infoLog);
+                Console.WriteLine(infoLog);
+                return;
+            }
+
+            uint fragShader = gl.CreateShader(GLEnum.FragmentShader);
+            string fragShaderSrc = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fShader.frag"));
+            gl.ShaderSource(fragShader, fragShaderSrc);
+            gl.CompileShader(fragShader);
+            if ((GLEnum)gl.GetShader(fragShader, GLEnum.CompileStatus) == GLEnum.False)
+            {
+                gl.GetShaderInfoLog(fragShader, 512, null, out string infoLog);
+                Console.WriteLine(infoLog);
+                return;
+            }
+
+            var shaderProgram = gl.CreateProgram();
+            gl.AttachShader(shaderProgram, vShader);
+            gl.AttachShader(shaderProgram, fragShader);
+            gl.LinkProgram(shaderProgram);
+            gl.UseProgram(shaderProgram);
+            gl.DeleteShader(vShader);
+            gl.DeleteShader(fragShader);
+            if ((GLEnum)gl.GetProgram(shaderProgram, GLEnum.LinkStatus) == GLEnum.False)
+            {
+                Console.WriteLine(gl.GetProgramInfoLog(shaderProgram));
+                return;
+            }
+
+            gl.VertexAttribPointer(0, 3, GLEnum.Float, normalized: false, stride: 3 * sizeof(float), 0);
+            gl.EnableVertexAttribArray(0);
         }
     }
 }
